d223cc5d5451c5572446d4223ee5bb5fee7224a0
djx <1442713848@qq.com>
2019-07-01 11:10:46 +0800

    This is my First java Class code file.

--- src/main/java/com/djx/javaClassFirst/JavaClassFirst.java
+++ src/main/java/com/djx/javaClassFirst/JavaClassFirst.java
@@ -5,5 +5,6 @@ public class JavaClassFirst {
     public static void main(String[] args) {
         System.out.println("It's very good project！");
         System.out.println("Thanks for you help!");
+        System.out.println("Thanks for you help!");
     }
 }
